5cb3c3fc9aa206d6d2c209dc39a3dac823cc67a3
Bill <25640332+Bilbottom@users.noreply.github.com>
2023-01-13 09:23:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import pandas as pd
 
 
 def main() -> None:
-    fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+    fruit_list = (
+        pd
+            .read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+            .set_index("Fruit")
+    )
     
-    streamlit.header('Breakfast Menu')
-    streamlit.text('Omega 3 & Blueberry Oatmeal')
-    streamlit.text('Kale, Spinach & Rocket Smoothie')
-    streamlit.text('Hard-Boiled Free-Range Egg')
-    streamlit.dataframe(fruit_list)
+    streamlit.header("Breakfast Menu")
+    streamlit.text("Omega 3 & Blueberry Oatmeal")
+    streamlit.text("Kale, Spinach & Rocket Smoothie")
+    streamlit.text("Hard-Boiled Free-Range Egg")
+    
+    fruit_selection = streamlit.multiselect("Pick some fruits:", list(fruit_list.index), ["Avocado", "Strawberries"])
+    streamlit.dataframe(fruit_list.loc[fruit_selection, :])
 
 
 if __name__ == "__main__":
