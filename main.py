5e99db99b2f240802914d0ebe64f3d145d70a6c1
Bill <25640332+Bilbottom@users.noreply.github.com>
2023-01-13 15:58:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
-import streamlit
+"""
+Simple streamlit application created as part of the Snowflake Data Application Builders Workshop.
+"""
 import pandas as pd
+import requests
+import streamlit
 
 
-def main() -> None:
+def add_text() -> None:
+    """
+    Add the text at the top of the application.
+    """
+    streamlit.header("Breakfast Menu")
+    streamlit.text("Omega 3 & Blueberry Oatmeal")
+    streamlit.text("Kale, Spinach & Rocket Smoothie")
+    streamlit.text("Hard-Boiled Free-Range Egg")
+
+    
+def add_fruit_selection() -> None:
+    """
+    Add a fruit table with a fruit picker above it.
+    """
     fruit_list = (
         pd
             .read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
             .set_index("Fruit")
     )
-    
-    streamlit.header("Breakfast Menu")
-    streamlit.text("Omega 3 & Blueberry Oatmeal")
-    streamlit.text("Kale, Spinach & Rocket Smoothie")
-    streamlit.text("Hard-Boiled Free-Range Egg")
-    
     fruit_selection = streamlit.multiselect("Pick some fruits:", list(fruit_list.index), ["Avocado", "Strawberries"])
     streamlit.dataframe(fruit_list.loc[fruit_selection, :])
+    
+
+def add_fruityvice_table() -> None:
+    """
+    Call the Fruityvice API and display the response.
+    """
+    streamlit.header("Fruityvice Data")
+    fruit_choice = streamlit.text_input("Which fruit would you like information about?", "Kiwi")
+    
+    streamlit.write(f"The user chose {fruit_choice}")
+    endpoint = f"https://fruityvice.com/api/fruit/{fruit_choice}"
+    
+    fruityvice_normalized = pd.json_normalize(requests.get(endpoint).json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+
+def main() -> None:
+    """
+    Entry point into the application.
+    """
+    add_text()
+    add_fruit_selection()
+    add_fruityvice_table()
 
 
 if __name__ == "__main__":
