af3b81ba575ada5d1aa261bb97c21b9fbbd984c2
Bill <25640332+Bilbottom@users.noreply.github.com>
2023-01-13 09:18:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import streamlit
+import pandas as pd
 
+
+def main() -> None:
+    fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+    
+    streamlit.header('Breakfast Menu')
+    streamlit.text('Omega 3 & Blueberry Oatmeal')
+    streamlit.text('Kale, Spinach & Rocket Smoothie')
+    streamlit.text('Hard-Boiled Free-Range Egg')
+    streamlit.dataframe(fruit_list)
+
+
+if __name__ == "__main__":
+    main()
